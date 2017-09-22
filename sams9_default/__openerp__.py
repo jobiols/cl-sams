696272c5af0b17c32055271b8a89349e24777aca
jobiols <jorge.obiols@gmail.com>
2017-09-22 14:48:30 -0300

    [ADD] commit inicial

--- sams9_default/__openerp__.py
+++ sams9_default/__openerp__.py
@@ -24,9 +24,7 @@
     'category': 'Tools',
     'summary': 'Customización Sams',
     'description': """
-.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
-   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
-   :alt: License: AGPL-3
+
 
 Customización Sams
 ==================
