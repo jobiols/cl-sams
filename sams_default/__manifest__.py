ca0c554c8db2ff1ea2c75eecda94710910e4cab4
jobiols <jorge.obiols@gmail.com>
2019-10-16 17:31:29 -0300

    [ADD] account-invoicing

--- sams_default/__manifest__.py
+++ sams_default/__manifest__.py
@@ -75,8 +75,9 @@
         {'usr': 'ingadhoc', 'repo': 'argentina-reporting',
          'branch': '11.0'},
         {'usr': 'ingadhoc', 'repo': 'stock', 'branch': '11.0'},
-        {'usr': 'oca', 'repo': 'sale-workflow', 'branch': '11.0'},
+        {'usr': 'ingadhoc', 'repo': 'account-invoicing', 'branch': '11.0'},
 
+        {'usr': 'oca', 'repo': 'sale-workflow', 'branch': '11.0'},
         {'usr': 'ingadhoc', 'repo': 'reporting-engine', 'branch': '11.0'},
         {'usr': 'ingadhoc', 'repo': 'aeroo_reports', 'branch': '11.0'},
         {'usr': 'jobiols', 'repo': 'oca-partner-contact', 'branch': '11.0'},
